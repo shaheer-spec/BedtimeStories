161415656570c5ccb951d59a90fc7e22f5ab26f2
Shaheer kayani <shaheer.kayani8@gmail.com>
2025-10-07 09:47:15 -0400

    Commit and Push with the code

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -1,4 +1,30 @@
 package com.pluralsight;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        try {
+
+            System.out.println("What bed time story do you want to read: ");
+            String fileName = scanner.nextLine();
+
+            BufferedReader reader = new BufferedReader(new FileReader(fileName));
+
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+
+            reader.close();
+        } catch (Exception ex) {
+            System.err.println("Wrong input, Try again.");
+        }
+
+    }
 }
